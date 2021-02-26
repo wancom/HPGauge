5b42483aa6ebf412dbb1bcd69482ec4c32776b8b
wancom <softpiware@gmail.com>
2021-02-26 10:30:15 +0900

    Add webpage

---
+++ docs/precache-manifest.d7e774e11095cd527ce8af3728507ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bbca24664bca7580ff",
+    "url": "/HPGauge/css/app.0ec4b13b.css"
+  },
+  {
+    "revision": "8c9241c2f4a9e310f2d6a37c4d0c2205",
+    "url": "/HPGauge/index.html"
+  },
+  {
+    "revision": "e1bbca24664bca7580ff",
+    "url": "/HPGauge/js/app.3815e279.js"
+  },
+  {
+    "revision": "a4317736a19634c96bb1",
+    "url": "/HPGauge/js/chunk-vendors.cf800635.js"
+  },
+  {
+    "revision": "f597b3de036173434f502af59dfde7a9",
+    "url": "/HPGauge/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/HPGauge/robots.txt"
+  }
+]);
